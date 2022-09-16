ff4819c96028c0955e278b8a7778b6eac01797b1
Hello-ImNew <baokhanhtk129@gmail.com>
2022-09-16 00:47:08 -0400

    Fourth Commit

--- app/src/main/java/com/example/project1_wordle/MainActivity.kt
+++ app/src/main/java/com/example/project1_wordle/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.project1_wordle
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.text.trimmedLength
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,9 +41,17 @@ class MainActivity : AppCompatActivity() {
         var ranWord = FourLetterWordList.getRandomFourLetterWord()
         correctGuess.text = ranWord
 
+        //check if the string is only contain alphabet letters
+        fun isAlpha(str: String): Boolean {
+            for (c in str) {
+                if (!c.isLetter()) return false
+            }
+            return true
+        }
+
         //make keyboard disappear
         fun closeKeyBoard() {
-            val view =  this.currentFocus
+            val view = this.currentFocus
             if (view != null) {
                 val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(view.windowToken, 0)
@@ -63,6 +77,41 @@ class MainActivity : AppCompatActivity() {
             correctGuess.text = ranWord
         }
 
+        //set the string with multiple colors
+        fun checkStringGen(answerCheck: TextView, guess: String, check: String) {
+            answerCheck.text = ""
+            for (i in 0..3) {
+                val word = SpannableString(guess[i].toString())
+                when (check[i]) {
+                    'O' -> {
+                        word.setSpan(
+                            ForegroundColorSpan(Color.GREEN),
+                            0,
+                            word.length,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    }
+                    'X' -> {
+                        word.setSpan(
+                            ForegroundColorSpan(Color.RED),
+                            0,
+                            word.length,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    }
+                    '+' -> {
+                        word.setSpan(
+                            ForegroundColorSpan(Color.parseColor("#ffd700")),
+                            0,
+                            word.length,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    }
+                }
+                answerCheck.append(word)
+            }
+        }
+
         //toggle between guess and reset button
         fun inputToReset() {
             correctGuess.visibility = View.VISIBLE
@@ -80,71 +129,98 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+        fun isContain(StringList: List<String>, str: String): Boolean {
+            for (s in StringList) {
+                if (s.equals(str, true)) {
+                    return true
+                }
+            }
+            return false
+        }
+
         fun inputButtonHandler() {
-            val guess = guessField.text.toString()
+            val guess = guessField.text.toString().uppercase()
             if (guess.length != 4) {
                 Toast.makeText(this, "Input Must Have 4 Characters", Toast.LENGTH_SHORT).show()
+                return
             }
-            else {
-                closeKeyBoard()
-                guessField.text.clear()
-                var check = ""
-                for (i in 0..3) {
-                    if (guess[i] == ranWord[i]) {
-                        check += "O"
-                    }
-                    else if (ranWord.contains(guess[i])) {
-                        check += "+"
-                    }
-                    else {
-                        check += "X"
-                    }
-                }
-                when (guessNum) {
-                    0 -> {
-                        answer1.text = guess
-                        answer1.visibility = View.VISIBLE
-                        guess1Check.visibility = View.VISIBLE
-                        answer1Check.text = check
-                        answer1Check.visibility = View.VISIBLE
-                        if (guess == ranWord) {
-                            inputToReset()
-                        }
-                        else {
-                            guess2.visibility = View.VISIBLE
-                            guessNum = 1
-                        }
-                    }
-                    1 -> {
-                        answer2.text = guess
-                        answer2.visibility = View.VISIBLE
-                        guess2Check.visibility = View.VISIBLE
-                        answer2Check.text = check
-                        answer2Check.visibility = View.VISIBLE
-                        if (guess == ranWord) {
-                            inputToReset()
-                        }
-                        else {
-                            guess3.visibility = View.VISIBLE
-                            guessNum = 2
-                        }
-                    }
-                    2 -> {
-                        answer3.text = guess
-                        answer3.visibility = View.VISIBLE
-                        guess3Check.visibility = View.VISIBLE
-                        answer3Check.text = check
-                        answer3Check.visibility = View.VISIBLE
-                        answer2.visibility = View.VISIBLE
-                        inputToReset()
-                    }
-                }
 
+            if (!isAlpha(guess)) {
+                Toast.makeText(this, "Contains characters that are not alphabetical", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            if (!isContain(FourLetterWordList.getAllFourLetterWords(), guess)) {
+                Toast.makeText(this, "Not in word list", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+
+            closeKeyBoard()
+            guessField.text.clear()
+            var check = ""
+            for (i in 0..3) {
+                check += if (guess[i] == ranWord[i]) {
+                    "O"
+                } else if (ranWord.contains(guess[i])) {
+                    "+"
+                } else {
+                    "X"
+                }
+            }
+            when (guessNum) {
+                0 -> {
+                    answer1.text = guess
+                    answer1.visibility = View.VISIBLE
+                    guess1Check.visibility = View.VISIBLE
+                    checkStringGen(answer1Check, guess, check)
+                    answer1Check.visibility = View.VISIBLE
+                    if (guess == ranWord) {
+                        inputToReset()
+                    } else {
+                        guess2.visibility = View.VISIBLE
+                        guessNum = 1
+                    }
+                }
+                1 -> {
+                    answer2.text = guess
+                    answer2.visibility = View.VISIBLE
+                    guess2Check.visibility = View.VISIBLE
+                    checkStringGen(answer2Check, guess, check)
+                    answer2Check.visibility = View.VISIBLE
+                    if (guess == ranWord) {
+                        inputToReset()
+                    } else {
+                        guess3.visibility = View.VISIBLE
+                        guessNum = 2
+                    }
+                }
+                2 -> {
+                    answer3.text = guess
+                    answer3.visibility = View.VISIBLE
+                    guess3Check.visibility = View.VISIBLE
+                    checkStringGen(answer3Check, guess, check)
+                    answer3Check.visibility = View.VISIBLE
+                    answer2.visibility = View.VISIBLE
+                    inputToReset()
+                }
             }
         }
-        inputButton.setOnClickListener{
+
+        //button click event listener
+        inputButton.setOnClickListener {
             inputButtonHandler()
         }
+
+        //edittext enter key event listener
+        guessField.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                inputButtonHandler()
+                return@OnKeyListener true
+            }
+            false
+        })
 
     }
 
